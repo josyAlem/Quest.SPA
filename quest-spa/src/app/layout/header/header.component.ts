38742b70b9c4eba37b7640c3733cc9c5e0a55cc4
josyAlem <yoseph.click@gmail.com>
2021-11-22 16:27:51 +0300

    working nav

--- quest-spa/src/app/layout/header/header.component.ts
+++ quest-spa/src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+   @Input() sidenav!: MatDrawer;
   constructor() { }
 
   ngOnInit(): void {
   }
-  openDrawer(){
+  sideNavToggle(){
 
-    
+    this.sidenav.mode="side";
+    this.sidenav.toggle();
   }
 }
