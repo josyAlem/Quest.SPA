384569aa813af12958cdcc07defac65035c9591c
josyAlem <yoseph.click@gmail.com>
2021-11-24 20:10:48 +0300

    Add: add side nav style

--- quest-spa/src/app/layout/side-nav/side-nav.component.ts
+++ quest-spa/src/app/layout/side-nav/side-nav.component.ts
@@ -6,12 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-
+  mainMenus: any;
+  extraMenus: any;
   constructor() { }
 
 
   ngOnInit(): void {
-
+    this.mainMenus = [
+      { name: "mainMenu1", icon: "folder", updated: "yes" },
+      { name: "mainMenu2", icon: "folder", updated: "yes" },
+      { name: "mainMenu3", icon: "folder", updated: "yes" },
+      { name: "mainMenu4", icon: "folder", updated: "yes" },
+      { name: "mainMenu5", icon: "folder", updated: "yes" },
+    ];
+    this.extraMenus = [
+      { name: "extraMenu1", icon: "folder", updated: "yes" },
+      { name: "extraMenu2", icon: "folder", updated: "yes" },
+      { name: "extraMenu3", icon: "folder", updated: "yes" },
+      { name: "extraMenu4", icon: "folder", updated: "yes" },
+      { name: "extraMenu5", icon: "folder", updated: "yes" },
+    ]
   }
 
 }
