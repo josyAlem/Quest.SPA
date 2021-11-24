dcaa8ce6f4fce7375ea5bcf413dce7442431d91e
josyAlem <yoseph.click@gmail.com>
2021-11-24 20:37:39 +0300

    Add: add route link logic on side-nav

--- quest-spa/src/app/layout/layout.module.ts
+++ quest-spa/src/app/layout/layout.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NgMaterialModule } from '../ng-material.module';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 
 @NgModule({
   declarations: [
-   HeaderComponent,
+    HeaderComponent,
     FooterComponent,
     SideNavComponent,
   ],
   imports: [
     CommonModule,
-    NgMaterialModule
+    NgMaterialModule,
+    RouterModule
   ],
-  exports:[HeaderComponent,
+  exports: [HeaderComponent,
     FooterComponent,
     SideNavComponent]
 })
--- quest-spa/src/app/layout/side-nav/side-nav.component.ts
+++ quest-spa/src/app/layout/side-nav/side-nav.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from "@angular/core";
-
+export interface sideNav {
+  name: string,
+  icon: string,
+  routerLink?: string
+}
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-  mainMenus: any;
-  extraMenus: any;
+  mainMenus: sideNav[] = [];
+  extraMenus: sideNav[] = [];
   constructor() { }
 
 
   ngOnInit(): void {
+    this.initSideMenu();
+  }
+
+  initSideMenu() {
     this.mainMenus = [
-      { name: "mainMenu1", icon: "folder", updated: "yes" },
-      { name: "mainMenu2", icon: "folder", updated: "yes" },
-      { name: "mainMenu3", icon: "folder", updated: "yes" },
-      { name: "mainMenu4", icon: "folder", updated: "yes" },
-      { name: "mainMenu5", icon: "folder", updated: "yes" },
+      { name: "mainMenu1", icon: "bookmark", routerLink: "auth" },
+      { name: "mainMenu2", icon: "bookmark" },
+      { name: "mainMenu3", icon: "bookmark" },
+      { name: "mainMenu4", icon: "bookmark" },
+      { name: "mainMenu5", icon: "bookmark" },
     ];
     this.extraMenus = [
-      { name: "extraMenu1", icon: "folder", updated: "yes" },
-      { name: "extraMenu2", icon: "folder", updated: "yes" },
-      { name: "extraMenu3", icon: "folder", updated: "yes" },
-      { name: "extraMenu4", icon: "folder", updated: "yes" },
-      { name: "extraMenu5", icon: "folder", updated: "yes" },
+      { name: "extraMenu1", icon: "bookmark" },
+      { name: "extraMenu2", icon: "bookmark" },
+      { name: "extraMenu3", icon: "bookmark" },
+      { name: "extraMenu4", icon: "bookmark" },
+      { name: "extraMenu5", icon: "bookmark" },
     ]
   }
 
